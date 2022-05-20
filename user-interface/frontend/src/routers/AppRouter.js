5e0dd2b38c1074d1cd3a81e995e2ba549f26c62b
liminghao <liminghao@people.cn>
2022-05-20 23:21:01 +0800

    fixed bug in AppRouter.js

--- user-interface/frontend/src/routers/AppRouter.js
+++ user-interface/frontend/src/routers/AppRouter.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Router, Route, Switch } from "react-router";
+import { Router, Route, Switch } from "react-router-dom";
 import { createBrowserHistory } from "history";
 import { NavProvider } from "../contexts/NavProvider";
 import PrivateRoute from "./PrivateRoute";
